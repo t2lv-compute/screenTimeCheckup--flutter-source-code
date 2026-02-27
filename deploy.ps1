806577b1dc7d2c82c381e0f90d8b9c4cbee82274
t2lv-compute <183874517+t2lv-compute@users.noreply.github.com>
2026-02-27 08:38:17 +0700

    Adding Automated deployment process

---
+++ deploy.ps1
@@ -0,0 +1,24 @@
+# Define script parameters
+param(
+    [Parameter(Mandatory=$true, HelpMessage="Enter the name of the commit")]
+    [string]$CommitName
+)
+
+echo "Starting deployment process for commit: $CommitName";
+echo "Pulling latest changes from the repository...";
+git pull origin main;
+echo "Commiting source code changes with commit message: $CommitName";
+git add .;
+git commit -m "$CommitName";
+echo "Pushing changes to the repository...";
+git push origin main;
+echo "Building the project for web at base-href /screentimecheckup/";
+flutter build web --base-href /screentimecheckup/;
+echo "Build process completed successfully!";
+cd screen_time_checkup/build/web;
+echo "Deploying to GitHub Pages with commit message: $CommitName";
+git add .;
+git commit -m "$CommitName";
+echo "Pushing changes to the repository...";
+git push origin main;
+echo "Deployment process completed successfully!";
